740cac454607b00c0dda42fbd151ced0eef9f802
Wojtek Wrona <w.wrona@selleo.com>
2016-02-21 21:37:46 +0100

    Add animated button

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 import React, {
   AppRegistry,
@@ -9,24 +5,46 @@ import React, {
   StyleSheet,
   Text,
   View,
-  TouchableHighlight
+  TouchableHighlight,
+  LayoutAnimation,
+  TouchableOpacity
 } from 'react-native';
 
 var IdleTimerManager = require('NativeModules').IdleTimerManager;
 
-var Button = React.createClass({
+var AnimatedButton = React.createClass({
+  componentWillMount() {
+    LayoutAnimation.spring();
+  },
+
+  getInitialState() {
+    return { w: 175, h: 175 }
+  },
+
+  _onPress() {
+    LayoutAnimation.spring();
+    this.setState({w: this.state.w + 15, h: this.state.h + 15})
+  },
+
+  _onLongPress() {
+    LayoutAnimation.spring();
+    this.setState({w: this.state.w - 15, h: this.state.h - 15})
+  },
+
   render: function() {
     return (
-      <TouchableHighlight
-        underlayColor={'white'}
-        style={styles.button}
-        onPress={this.props.onPress}
-        underlayColor='blue'
-        activeOpacity={0.4}>
-        <Text style={styles.buttonLabel}>
-          {this.props.label}
-        </Text>
-      </TouchableHighlight>
+      <View style={styles.animatedButton}>
+        <View style={[styles.box, {width: this.state.w, height: this.state.h}]}>
+          {this.props.children}
+        </View>
+        <TouchableOpacity
+        onPress={this._onPress}
+        onLongPress={this._onLongPress}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>{this.props.label}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     );
   }
 });
@@ -43,18 +61,19 @@ class TestGame extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-          {'\n'}
-        </Text>
-        <Button label="Super cool button" />
+        <AnimatedButton label="Press me!">
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.ios.js
+          </Text>
+          <Text style={styles.instructions}>
+            Press Cmd+R to reload,{'\n'}
+            Cmd+D or shake for dev menu
+            {'\n'}
+          </Text>
+        </AnimatedButton>
       </View>
     );
   }
@@ -63,28 +82,40 @@ class TestGame extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'white',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    color: 'white',
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+    color: 'white',
+  },
+  animatedButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    backgroundColor: 'black',
+    marginBottom: 10,
   },
   button: {
+    padding: 10,
+    backgroundColor: 'black',
+    alignItems: 'center',
   },
-  buttonLabel: {
-    color: 'blue',
-    backgroundColor: 'yellow',
-    borderColor: 'red',
-    borderWidth: 1,
-    padding: 5,
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
 
